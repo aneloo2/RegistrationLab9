4d5e6ef37d9d435f703dcc9ffcf8355b159ea95a
Anel Adilkhan <80376247+aneloo2@users.noreply.github.com>
2021-11-26 07:12:38 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+$(function() {
+  $("form[name='registration']").validate({
+    
+    rules: {
+      text: "required",
+      password: "required",
+      email: {
+        required: true,
+        // Specify that email should be validated
+        // by the built-in "email" rule
+        email: true
+      },
+      password: {
+        required: true,
+        minlength: 5
+      }
+    },
+    // Specify validation error messages
+    messages: {
+      text: "Please enter your username",
+      password: "password",
+      password: {
+        required: "Please provide a password",
+        minlength: "Your password must be at least 5 characters long"
+      },
+      email: "Please enter a valid email address"
+    },
+    // Make sure the form is submitted to the destination defined
+    // in the "action" attribute of the form when valid
+    submitHandler: function(form) {
+      form.submit();
+    }
+  });
+});
+
